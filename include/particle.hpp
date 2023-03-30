7dd891eb114152a1596c0239d5b67d9eb537ed38
shameer2001 <shameer2001@github.com>
2023-03-30 16:20:05 +0000

    Enable `ctest`, define get, update functions and constructors

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,8 +1,24 @@
+#include <Eigen/Core>
+
 class Particle {
     public:
+        // Constructors:
         Particle(double in_mass);
+        Particle(double in_mass, Eigen::Vector3d &in_pos, Eigen::Vector3d &in_vel, Eigen::Vector3d &in_acc); 
+
+        // get functions:
+        Eigen::Vector3d getPosition() const;
+        Eigen::Vector3d getVelocity() const;
         double getMass() const;
+
+        // Update function:
+        void update(double dt);
 
     private:
         double mass;
+        Eigen::Vector3d position;
+        Eigen::Vector3d velocity;
+        Eigen::Vector3d acceleration;
+        
+
 };
--- src/particle.cpp
+++ src/particle.cpp
@@ -1,10 +1,25 @@
 #include "particle.hpp"
-#include <Eigen/Core>
 
 Particle::Particle(double in_mass) :
     mass{in_mass}
     {}
+Particle::Particle(double in_mass, Eigen::Vector3d &in_pos, Eigen::Vector3d &in_vel, Eigen::Vector3d &in_acc) :
+    mass{in_mass}, position{in_pos}, velocity{in_vel}, acceleration{in_acc}
+    {}  
+
 
 double Particle::getMass() const {
     return mass;
+}
+Eigen::Vector3d Particle::getPosition() const {
+    return position;
+}
+Eigen::Vector3d Particle::getVelocity() const {
+    return velocity;
+}
+
+
+void Particle::update(double dt) {
+    position += dt * velocity;
+    velocity += dt * acceleration;
 }
