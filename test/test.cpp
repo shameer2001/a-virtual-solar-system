bd519065e9c385770e123c52e43d8b1a4857f3bd
shameer2001 <shameer2001@github.com>
2023-03-31 07:09:03 +0000

    Unit tests for calcAcceleration() and sumAccelerations() functions

--- test/test.cpp
+++ test/test.cpp
@@ -75,3 +75,173 @@ TEST_CASE( "Particle calculates velocity and postion correctly with artificial a
 
 
 
+TEST_CASE("The gravitational force/acceleration between two particles is as expected: calcAcceleration function", "[particle]") {
+
+    SECTION("Test case 1; simple", "[particles]") {
+        Eigen::Vector3d pos_test1(0, 0, 0);
+        Eigen::Vector3d vel_test1(0, 0, 0);
+        Eigen::Vector3d acc_test1(1, 1, 1);
+        double mass_test1 = 1;
+
+        Eigen::Vector3d pos_test2(1, 1, 1);
+        Eigen::Vector3d vel_test2(0, 0, 0);
+        Eigen::Vector3d acc_test2(0, 0, 0);
+        double mass_test2 = 1;
+
+        Particle p1(mass_test1, pos_test1, vel_test1, acc_test1);
+        Particle p2(mass_test2, pos_test2, vel_test2, acc_test2);
+
+        Eigen::Vector3d acceleration = calcAcceleration(p1, p2);
+        Eigen::Vector3d acc_exp(1.0/pow(3.0, 3.0/2.0), 1.0/pow(3.0, 3.0/2.0), 1.0/pow(3.0, 3.0/2.0));
+        std::cout << acceleration[0] << " " << acceleration[1] << " " << acceleration[2] << std::endl;
+        std::cout << acc_exp[0]  << " " << acc_exp[1]  << " " << acc_exp[2] << std::endl;
+        std::cout << 1/3;
+
+        REQUIRE(acceleration.isApprox(acc_exp));
+    }
+
+
+    SECTION("Test case 2; different xyz", "[particles]") {
+        Eigen::Vector3d pos_test1(0, 0, 0);
+        Eigen::Vector3d vel_test1(2, 3, 1);
+        Eigen::Vector3d acc_test1(4, 0, 1);
+        double mass_test1 = 4;
+
+        Eigen::Vector3d pos_test2(1, 1, 2);
+        Eigen::Vector3d vel_test2(4, 5, 7);
+        Eigen::Vector3d acc_test2(1, 2, 3);
+        double mass_test2 = 1;
+
+        Particle p1(mass_test1, pos_test1, vel_test1, acc_test1);
+        Particle p2(mass_test2, pos_test2, vel_test2, acc_test2);
+
+        Eigen::Vector3d acceleration = calcAcceleration(p1, p2);
+        Eigen::Vector3d acc_exp( 1.0/pow(6, 3.0/2.0), 1.0/pow(6, 3.0/2.0), 2.0/pow(6, 3.0/2.0) );
+
+        REQUIRE(acceleration.isApprox(acc_exp));
+    }
+
+
+
+    SECTION("Test case 3; negative position", "[particles]") {
+        Eigen::Vector3d pos_test1(-4, -2, -5);
+        Eigen::Vector3d vel_test1(2, 3, 1);
+        Eigen::Vector3d acc_test1(4, 0, 1);
+        double mass_test1 = 4;
+
+        Eigen::Vector3d pos_test2(-1, -1, -2);
+        Eigen::Vector3d vel_test2(4, 5, 7);
+        Eigen::Vector3d acc_test2(1, 2, 3);
+        double mass_test2 = 1;
+
+        Particle p1(mass_test1, pos_test1, vel_test1, acc_test1);
+        Particle p2(mass_test2, pos_test2, vel_test2, acc_test2);
+
+        Eigen::Vector3d acceleration = calcAcceleration(p1, p2);
+        Eigen::Vector3d acc_exp( 3.0/pow(19.0, 3.0/2.0), 1.0/pow(19.0, 3.0/2.0), 3.0/pow(19.0, 3.0/2.0) );
+
+        REQUIRE(acceleration.isApprox(acc_exp));
+    }
+
+
+
+    SECTION("Test case 4; negative positive", "[particles]") {
+        Eigen::Vector3d pos_test1(-4, -2, -5);
+        Eigen::Vector3d vel_test1(2, 3, 1);
+        Eigen::Vector3d acc_test1(4, 0, 1);
+        double mass_test1 = 4;
+
+        Eigen::Vector3d pos_test2(1, 1, 2);
+        Eigen::Vector3d vel_test2(4, 5, 7);
+        Eigen::Vector3d acc_test2(1, 2, 3);
+        double mass_test2 = 1;
+
+        Particle p1(mass_test1, pos_test1, vel_test1, acc_test1);
+        Particle p2(mass_test2, pos_test2, vel_test2, acc_test2);
+
+        Eigen::Vector3d acceleration = calcAcceleration(p1, p2);
+        Eigen::Vector3d acc_exp( 5.0/pow(83.0, 3.0/2.0), 3.0/pow(83.0, 3.0/2.0), 7.0/pow(83.0, 3.0/2.0) );
+
+        REQUIRE(acceleration.isApprox(acc_exp));
+    }
+
+}
+
+
+
+
+
+
+
+
+TEST_CASE("Ensure that a list of particles that includes itself to a particle will not add any acceleration: sumAccelerations function", "[particle]") {
+
+    Eigen::Vector3d pos_test1(0, 0, 0);
+    Eigen::Vector3d vel_test1(2, 3, 1);
+    Eigen::Vector3d acc_test1(-4, -2, -1);
+    double mass_test1 = 4;
+
+    Eigen::Vector3d pos_test2(1, 1, 2);
+    Eigen::Vector3d vel_test2(4, 5, 7);
+    Eigen::Vector3d acc_test2(-1, -2, -3);
+    double mass_test2 = 1;
+
+    Eigen::Vector3d pos_test3(2, 2, 2);
+    Eigen::Vector3d vel_test3(5, 5, 5);
+    Eigen::Vector3d acc_test3(1, 2, 3);
+    double mass_test3 = 3;
+
+
+    Particle p1{mass_test1, pos_test1, vel_test1, acc_test1};
+    Particle p2{mass_test2, pos_test2, vel_test2, acc_test2};
+    Particle p3{mass_test3, pos_test3, vel_test3, acc_test3};
+
+    std::vector<Particle> list_with_self{p1, p2, p3};
+    std::vector<Particle> list_wout_self{p2, p3};
+
+    sumAccelerations(list_with_self, p1);
+    const Particle p1_with_self = p1;
+
+    sumAccelerations(list_wout_self, p1);
+    const Particle p1_wout_self = p1;
+
+
+    REQUIRE( p1_with_self.getAcceleration() == p1_wout_self.getAcceleration() );
+
+
+}
+
+
+
+
+
+TEST_CASE("Ensure a particle with two equally sized particles an equal distance away will have 0 acceleration: sumAccelerations function", "[particle]") {
+
+    Eigen::Vector3d pos_test1(0, 0, 0);
+    Eigen::Vector3d vel_test1(2, 3, 1);
+    Eigen::Vector3d acc_test1(-4, -2, -1);
+    double mass_test1 = 4;
+
+    Eigen::Vector3d pos_test2(1, 0, 0);
+    Eigen::Vector3d vel_test2(5, 0, 0);
+    Eigen::Vector3d acc_test2(2, 0, 0);
+    double mass_test2 = 2;
+
+    Eigen::Vector3d pos_test3(-1, 0, 0);
+    Eigen::Vector3d vel_test3(5, 0, 0);
+    Eigen::Vector3d acc_test3(2, 0, 0);
+    double mass_test3 = 2;
+
+
+    Particle p1{mass_test1, pos_test1, vel_test1, acc_test1};
+    Particle p2{mass_test2, pos_test2, vel_test2, acc_test2};
+    Particle p3{mass_test3, pos_test3, vel_test3, acc_test3};
+
+    std::vector<Particle> list{p1, p2, p3};
+
+    sumAccelerations(list, p1);
+    Eigen::Vector3d acc_zero(0, 0, 0);
+
+
+    REQUIRE( p1.getAcceleration() == acc_zero );
+}
