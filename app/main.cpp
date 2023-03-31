fc498a2f5ed0895457eb063f3cfadabde9531538
shameer2001 <shameer2001@github.com>
2023-03-31 08:47:44 +0000

    Create command line app arguments and help message

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,75 @@
 #include <iostream>
-#include <Eigen/Core>
+#include "particle.hpp"
 
-int main() {
-  Eigen::Vector3d v{1, 0, 0};
-  
-  std::cout << "Hello World!\n" << v << std::endl;
+
+void help() {
+  std::cout << "Usage: solarSystemSimulator [options]\n"
+            << "Options:\n"
+            << "  -t, --timestep           Set the timestep\n"
+            << "  -s, --simulation-time    Set the total simulation time\n"
+            << "  -h, --help               Show this help message\n";
+
+}
+
+
+
+int main(int argc, char *argv[]) 
+{
+  // Parse command line arguments
+  bool dt = 0.0;
+  bool sim_time = 0.0;
+
+  for (int i = 1; i < argc; i++)
+  {
+    std::string arg = argv[i];
+
+
+
+    if (arg == "-t" || arg == "--timestep")
+    {
+      if (i + 1 < argc) 
+      {
+        dt = argv[i + 1]; // Argument after --t is the filename
+        i++;
+      }
+      else 
+      {
+        help();
+        return 1;
+      }
+    
+    }
+
+
+    else if (arg == "-s" || arg == "--simulation-time" && (i+1)<argc)
+    {
+      if (i + 1 < argc) 
+      {
+        sim_time = argv[i + 1]; // Argument after --t is the filename
+        i++;
+      }
+      else 
+      {
+        help();
+        return 1;
+      }
+    }
+
+
+    else if (arg == "-h" || arg == "--help")
+    {
+      help();
+      return 0;
+    }
+
+
+    else 
+    {
+      help();
+      return 0;
+    }
+
+  }
 
   return 0;
 }
