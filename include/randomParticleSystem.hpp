f7e167eed02b36ec485cb805b82354c328c3008e
shameer2001 <shameer2001@github.com>
2023-04-08 07:46:39 +0000

    Create random generator as subclass. Implement evolution of system

---
+++ include/randomParticleSystem.hpp
@@ -0,0 +1,42 @@
+#ifndef randomParticleSystem_hpp
+#define randomParticleSystem_hpp
+
+#include "solarSystem.hpp"
+
+
+
+class RandomSystem : public InitialConditionGenerator  // Refactor class as subclass for base class InitialConditionGenerator
+{
+
+  public:
+  RandomSystem(int body_num);
+  
+  Particle celestialBody(double mass, double distance, double angle = 0.0); // Mass is relative to the sun and distance is that between body and sun
+
+  std::vector<std::shared_ptr<Particle>> generateInitialConditions() override; 
+  std::vector<std::shared_ptr<Particle>> getCelestialBodyList();
+
+  void evolutionOfRandomSystem(double dt, double total_time);
+  void printMessages();
+
+
+  double totalKineticEnergy();
+  double totalPotentialEnergy();
+  double totalEnergy();
+
+  void printEnergyMessages();
+
+
+  private:
+  int num_bodies;
+  std::vector<std::shared_ptr<Particle>> celestial_body_list;
+
+
+
+
+};
+
+
+
+
+#endif
---
+++ src/randomParticleSystem.cpp
@@ -0,0 +1,81 @@
+#include "randomParticleSystem.hpp"
+#include <chrono>
+#include <random>
+
+
+RandomSystem::RandomSystem(int body_num): num_bodies(body_num) {}
+
+Particle RandomSystem::celestialBody(double mass, double distance, double angle) {
+    if (mass == 1.0 && distance == 0.0) // A central star
+    {
+        Eigen::Vector3d pos_star(distance, distance, distance);
+        Eigen::Vector3d vel_star(0.0, 0.0, 0.0);
+        Eigen::Vector3d acc_star(0.0, 0.0, 0.0);
+
+        return Particle {mass, pos_star, vel_star, acc_star};
+    }
+    else {
+
+        Eigen::Vector3d pos(distance * sin(angle),  distance * cos(angle),  0.0);
+        Eigen::Vector3d vel((-1.0/sqrt(distance)) * cos(angle),  (1.0/sqrt(distance)) * sin(angle), 0.0);
+        Eigen::Vector3d acc(0.0, 0.0, 0.0); // Initial acceleration is 0 or all bodies
+
+        return Particle {mass, pos, vel, acc};
+    }
+}
+
+std::vector<std::shared_ptr<Particle>> RandomSystem::generateInitialConditions() {
+    celestial_body_list.clear(); // Ensure list is empty
+
+    auto star = std::make_shared<Particle>(celestialBody(1.0, 0.0));
+    celestial_body_list.push_back(star);
+
+    std::default_random_engine generator(42); // Seed the random number generator
+    std::uniform_real_distribution<double> massDistribution(1.0 / 6000000, 1.0 / 1000);
+    std::uniform_real_distribution<double> distanceDistribution(0.4, 30.0);
+    std::uniform_real_distribution<double> angleDistribution(0.0, 2.0 * M_PI);
+
+    // Create the remaining particles
+    for (unsigned int i = 1; i < num_bodies; i++) {
+
+        // Generate random values:
+        double mass = massDistribution(generator);
+        double distance = distanceDistribution(generator);
+        double angle = angleDistribution(generator);
+
+        auto body = std::make_shared<Particle>(celestialBody(mass, distance, angle)); // Make celestial body instance as shared pointers
+        celestial_body_list.push_back(body);
+    }
+
+    return celestial_body_list;
+}
+
+
+std::vector<std::shared_ptr<Particle>> RandomSystem::getCelestialBodyList() {
+    return celestial_body_list; 
+}
+
+
+void RandomSystem::evolutionOfRandomSystem(double dt, double total_time) {
+
+    // Loop for full simulation time
+    for (double sim_time = 0.0; sim_time < total_time; sim_time += dt) {
+
+        // Update acceleration felt by each body
+        for (auto& particle : celestial_body_list) {
+            sumAccelerations(celestial_body_list, *particle);
+        }
+        // Update position and velocity of each body
+        for (auto& particle : celestial_body_list) {
+            particle->update(dt);
+        }
+    }
+}
+
+
+
+void RandomSystem::printMessages() {}
+
+
+
+
