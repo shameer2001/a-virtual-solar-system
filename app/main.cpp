49533fdf7066fd28651067b27616c022a7dac5a7
shameer2001 <shameer2001@github.com>
2023-04-14 18:15:41 +0000

    Implement correct runtime calculations for 8 timesteps as per pdf. Allow 'pi' as user input.

--- app/main.cpp
+++ app/main.cpp
@@ -8,15 +8,22 @@
 
 void help() {
   std::cout << "Usage: solarSystemSimulator [options]\n"
+            << " \n"
             << "Options:\n"
-            << "  -ss,  --solar-system       To simulate our solar system.\n"
-            << "  -rs,  --random-system      To simulate a random solar system with random bodies.\n"
-            << "  -n,   --number             Set the number of bodies in the random system.\n"
-            << "  -e,   --epsilon            Set the softening factor for the random system. Default is 0.0.\n"
-            << "  -t,   --timestep           Set the timestep of the simulation.\n"
-            << "  -s,   --simulation-time    Set the total simulation time.\n"
+            << "  -ss,  --solar_system       Select to simulate our solar system.\n"
+            << "  -rs,  --random_system      Select to simulate a random solar system with random bodies.\n"
+            << "  -n,   --number             Set the number of bodies in the random system (must select random system first). Type is integer.\n"
+            << "  -e,   --epsilon            Set the softening factor for the random system (must select random system first). Type is double. Default is 0.0.\n"
+            << "  -t,   --timestep           Set the timestep of the simulation. Type is double.\n"
+            << "  -s,   --simulation_time    Set the total simulation time. Type is double.\n"
+            << "  -r,   --runtime            Select if you want measurements of the runtime of the simulation.\n"
             << "  -h,   --help               Show this help message.\n"
-            << "Note: The units for the time arguments are in radians where 2π represents one full earth cycle (i.e. one year).";
+            << " \n"
+            << "Note 1 : The units for the time arguments are in radians where 2π represents one full earth cycle (i.e. one year).\n"
+            << "Note 2 : You can use 'pi' in the simulation time argument to input a precise value of π and multiply it with any number BEFORE 'pi'\n"
+            << "         E.G. 2pi = 2π\n"          
+            << " "
+            << std::endl;
 }
 
 
@@ -30,10 +37,12 @@ int main(int argc, char *argv[])
   double soft_fac = 0.0;
   double dt = 0.0;
   double sim_time = 0.0;
+  bool run_time = false; // To measure runtime or not to measure runtime
 
   if (argc == 1) // When there are no arguments given
   {
     help();
+    throw std::runtime_error("No inputs given"); 
     return 1;
   }
 
@@ -41,7 +50,7 @@ int main(int argc, char *argv[])
   {
     std::string arg = argv[i];
 
-    if (arg == "-ss" || arg == "--solar-system") {
+    if (arg == "-ss" || arg == "--solar_system" && (i+1)<argc) {
       if (i + 1 < argc) // If there are arguments after this one
       {
         solarsystem = true;
@@ -52,7 +61,7 @@ int main(int argc, char *argv[])
         return 1;
       }
     }
-    else if (arg == "-rs" || arg == "--random-system") {
+    else if (arg == "-rs" || arg == "--random_system" && (i+1)<argc) {
       if (i + 1 < argc) 
       {
         randomsystem = true;
@@ -64,7 +73,7 @@ int main(int argc, char *argv[])
       }    
     }
 
-    else if (arg == "-n" || arg == "--number") {
+    else if (arg == "-n" || arg == "--number" && (i+1)<argc) {
       if (i + 1 < argc) // If there is a value after this argument
       {
         num_bodies = std::stoi(argv[i + 1]); 
@@ -75,9 +84,14 @@ int main(int argc, char *argv[])
         help();
         return 1;
       }
+      if (randomsystem != true) {
+        help();
+        throw std::runtime_error("Did not select to simulate random system so cannot use -n or --number"); 
+        return 1;
+      }
     }
 
-    else if (arg == "-e" || arg == "--epsilon")
+    else if (arg == "-e" || arg == "--epsilon" && (i+1)<argc)
     {
       if (i + 1 < argc)
       {
@@ -89,9 +103,14 @@ int main(int argc, char *argv[])
         help();
         return 1;
       }
+      if (randomsystem != true) {
+        help();
+        throw std::runtime_error("Did not select to simulate random systems so cannot use -e or --epsilon"); 
+        return 1;
+      }
     }
 
-    else if (arg == "-t" || arg == "--timestep")
+    else if (arg == "-t" || arg == "--timestep" && (i+1)<argc)
     {
       if (i + 1 < argc)
       {
@@ -106,18 +125,61 @@ int main(int argc, char *argv[])
     }
 
 
-    else if (arg == "-s" || arg == "--simulation-time" && (i+1)<argc)
+    else if (arg == "-s" || arg == "--simulation_time" && (i+1)<argc)
     {
       if (i + 1 < argc) 
       {
-        sim_time = strtod(argv[i + 1], NULL); 
+        std::string sim_time_str(argv[i + 1]); // Convert the input to string
+        size_t pi_pos = sim_time_str.find("pi"); // Find position of "pi"
+
+        // If no "pi" found in argument
+        if (pi_pos == std::string::npos) { 
+
+          const char* input = argv[i + 1];
+          char* endptr;
+          sim_time = strtod(input, &endptr); // Simulation time as input if input is a double
+
+          if (*endptr != '\0') { // If non-numerical character in argument
+            help();
+            throw std::runtime_error("Invalid character encountered in simulation time argument. The only non-numerical characters allowed are 'pi'");
+          }
+        }  
+
+        else {
+          std::string x_str = sim_time_str.substr(0, pi_pos); // String between position 0 and pi_pos (ie any "coefficient" of pi)
+
+          if (x_str == "") { // If no coefficient
+            sim_time = M_PI;
+          }
+          else {
+            double x = std::stod(x_str);
+            sim_time = x * M_PI;
+          }
+        }
+
         i++;
       }
+
+
       else 
       {
         help();
         return 1;
       }
+    }
+
+
+
+    else if (arg == "-r" || arg == "--runtime") {
+      if (i < argc) 
+      {
+        run_time = true;
+      }
+      else 
+      {
+        help();
+        return 1;
+      }    
     }
 
 
@@ -133,6 +195,9 @@ int main(int argc, char *argv[])
 
 
 
+
+
+
   // Use pointer to base class generateInitialConditions method instead of calling from subclasses
   // This will reduce code duplication and reduce memory usage
   InitialConditionGenerator* systems[2]; // Pointer to initial condition generator base class
@@ -141,115 +206,50 @@ int main(int argc, char *argv[])
   if (solarsystem == true) {
 
     systems[0] = new SolarSystem(); // Create object of SolarSystem class
-    SolarSystem* solar_system = dynamic_cast<SolarSystem*>(systems[0]); // Cast the already defined InitialConditionGenerator Pointer to a SolarSystem pointer. i.e. you can use solar_system as a pointer to SolarSystem and use the other methods exclusive to the SolarSystem class
-
-    // Simulate solar system and it's evolution
-    systems[0]->generateInitialConditions();
-    solar_system->printMessages(); 
-    printEnergyMessages(solar_system->getCelestialBodyList());
-
-    evolutionOfSystem(solar_system->getCelestialBodyList(), dt, sim_time); // Run evolution simulation
-    solar_system->printMessages();
-    printEnergyMessages(solar_system->getCelestialBodyList());
+    SolarSystem* solar_system = dynamic_cast<SolarSystem*>(systems[0]); // Cast the already defined InitialConditionGenerator Pointer to a SolarSystem pointer. 
+    // i.e. you can use solar_system as a pointer to SolarSystem and use the other methods exclusive to the SolarSystem class
 
 
-
-
-    // Run simulation for 1 year (2pi) and dt = 0.001:
-    std::cout << "=======Running the simulation for one year and with a dt of 0.001=======\n" << std::endl;
-    systems[0]->generateInitialConditions();
-    solar_system->printMessages();
-    evolutionOfSystem(solar_system->getCelestialBodyList(), 0.001, 2 * M_PI); 
-    solar_system->printMessages();
-
-
-
-
-    // Run simulation for 1 year (2pi) and 8 different timesteps and print energies:
-    std::vector<double> dt_list{0.0001, 0.001, 0.01, 0.1, 1.0, 62.8, 100.0, 314.0};
-    std::cout << "=======Run simulation for 1 year and 8 different timesteps and PRINT ENERGIES=======\n" << std::endl;
-
-    for(double& dt: dt_list) {
-      std::cout << "-------Running for one year and with dt = " << dt << "-------\n" << std::endl;
-
+    if (run_time == false) {
+      // Simulate solar system and it's evolution
       systems[0]->generateInitialConditions();
       solar_system->printMessages(); 
       printEnergyMessages(solar_system->getCelestialBodyList());
 
-
-      evolutionOfSystem(solar_system->getCelestialBodyList(), dt, 2 * M_PI); 
+      evolutionOfSystem(solar_system->getCelestialBodyList(), dt, sim_time); // Run evolution simulation
       solar_system->printMessages();
       printEnergyMessages(solar_system->getCelestialBodyList());
-
     }
 
 
-
-
-    // Run simulation for 1 year (2pi) and 8 different timesteps and print simulation time:
-    double total_runtime = 0.0;
-    std::cout << "=======Run simulation for 1 year and 8 different timesteps and PRINT SIMULATION TIME=======\n" << std::endl;
-    
-    for(double& dt: dt_list) {
-      std::cout << "-------Running for one year and with dt = " << dt << " -------\n" <<std::endl;
-
+    // If you want to measure runtime
+    else {
       auto start_time = std::chrono::high_resolution_clock::now();
       systems[0]->generateInitialConditions();
 
-      #ifdef DEBUG // Disable output when debug enabled in order to prevent inclusion in simulation time calculation
+      #ifdef DEBUG // Disable
       solar_system->printMessages(); 
       printEnergyMessages(solar_system->getCelestialBodyList());
-
       #endif
 
-      evolutionOfSystem(solar_system->getCelestialBodyList(), dt, 2 * M_PI); 
+      evolutionOfSystem(solar_system->getCelestialBodyList(), dt, sim_time); 
       auto end_time = std::chrono::high_resolution_clock::now();
 
       solar_system->printMessages();
       printEnergyMessages(solar_system->getCelestialBodyList());
 
+      int num_timesteps = std::ceil( sim_time / dt ); // Number of timesteps needed in simulation (round up to nearest int)
 
       double runtime = std::chrono::duration<double, std::milli>(end_time - start_time).count();
-      std::cout << "The runtime of this simulation is " 
-                << runtime << " ms\n"
+      std::cout << "The total simulation time is: " << runtime << " ms\n"
+                << "The average time per timestep is: " << runtime/num_timesteps << " ms\n"         
       << std::endl;
-
-      total_runtime += runtime;
     }
 
-    std::cout << "The total simulation time is: " << total_runtime << " ms\n"
-              << "The average simulation time is: " << total_runtime / solar_system->getCelestialBodyList().size() << " ms\n"
-    << std::endl;
-
-
-
-
-
-    // Run simulation for 1 year (2pi) and dt = 0.01 and print simulation time (for 0 optimisation flags):
-    std::cout << "=======Running for one year and with dt = 0.01=======\n" <<std::endl;
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-    systems[0]->generateInitialConditions();
-
-    #ifdef DEBUG
-    solar_system->printMessages(); 
-    printEnergyMessages(solar_system->getCelestialBodyList());
-
-    #endif
-
-    evolutionOfSystem(solar_system->getCelestialBodyList(), 0.01, 2 * M_PI); 
-    auto end_time = std::chrono::high_resolution_clock::now();
-
-    solar_system->printMessages();
-    printEnergyMessages(solar_system->getCelestialBodyList());
-
-
-    double runtime = std::chrono::duration<double, std::milli>(end_time - start_time).count();
-    std::cout << "The runtime of this simulation is " 
-              << runtime << " ms\n"
-    << std::endl;
-
   }
+
+
+
 
 
 
@@ -258,33 +258,63 @@ int main(int argc, char *argv[])
     systems[1] = new RandomSystem(num_bodies); // Create object of RandomSystem class
     RandomSystem* random_system = dynamic_cast<RandomSystem*>(systems[1]); // Cast the already defined InitialConditionGenerator Pointer to a RandomSystem pointer
 
-    // Simulate random system and it's evolution:
+    if (run_time == false) {
+      // Simulate random system and it's evolution:
+      systems[1]->generateInitialConditions();
+      printEnergyMessages(random_system->getCelestialBodyList());
 
-    systems[1]->generateInitialConditions();
-    printEnergyMessages(random_system->getCelestialBodyList());
-
-
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-    
-    systems[1]->generateInitialConditions(); // Generate again to include in runtime (without excluding energy messages)
-    evolutionOfSystem(random_system->getCelestialBodyList(), dt, sim_time, soft_fac); // Run evolution simulation
-    
-    auto end_time = std::chrono::high_resolution_clock::now();
+      evolutionOfSystem(random_system->getCelestialBodyList(), dt, sim_time, soft_fac); // Run evolution simulation
+      printEnergyMessages(random_system->getCelestialBodyList());
+    }
 
 
+    else {
+      auto start_time = std::chrono::high_resolution_clock::now();
+      systems[1]->generateInitialConditions();
 
-    printEnergyMessages(random_system->getCelestialBodyList());
+      #ifdef DEBUG
+      printEnergyMessages(random_system->getCelestialBodyList()); 
+      #endif
 
-    double runtime = std::chrono::duration<double, std::milli>(end_time - start_time).count();
-    std::cout << "The runtime of this simulation is " 
-              << runtime << " ms\n"
-    << std::endl;
+      evolutionOfSystem(random_system->getCelestialBodyList(), dt, sim_time, soft_fac); 
+      auto end_time = std::chrono::high_resolution_clock::now();
+
+      printEnergyMessages(random_system->getCelestialBodyList());  
+
+      int num_timesteps = std::ceil( sim_time / dt ); 
+
+      double runtime = std::chrono::duration<double, std::milli>(end_time - start_time).count();
+      std::cout << "The total simulation time is: " << runtime << " ms\n"
+                << "The average time per timestep is: " << runtime/num_timesteps << " ms\n"         
+      << std::endl;
+    }
+
+
   }
 
 
 
 
+
+
+
+  // If neither system is selected
+  else if (solarsystem == false && randomsystem == false) {
+    std::cout << "Did not select any system.\n" 
+              << " ";
+    help();
+    return 1;
+  }
+  // If both systems are selected
+  else if (solarsystem == true && randomsystem == true) {
+    std::cout << "Both systems cannot be simulated at the same time.\n" 
+              << " ";
+    help();
+    return 1;
+  }
+
+
+    
   return 0;
 }
 
